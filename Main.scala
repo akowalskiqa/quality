3616de14e150b3682be40a0d49101cb3dd71a02f
Administrator <arkadiusk.kowalski.qa.com>
2017-07-18 15:24:51 +0100

    completed

--- Main.scala
+++ Main.scala
@@ -1,28 +1,38 @@
-/**
-  * Created by Administrator on 18/07/2017.
-  */
+
 object Main {
   def main(args:Array[String]): Unit ={
-    val numbers = Array(Tuple3(6,"million","million"),
-      Tuple3(9,"billion","milliard"),
-      Tuple3(12,"trillion","billion"),
-      Tuple3(15,"quadrillion","billiard"),
-      Tuple3(18,"quintillion","trillion"),
-      Tuple3(21,"sextillion","trilliard")
-      )
-
     var input = scala.io.StdIn.readLine()
-      var inputLen = input.length
-    def test(iteration:Int): Unit ={
-      if(inputLen > numbers(iteration)._1){
-        test(iteration+1)
-      }else{
-        println("Your number in short scale is: "+numbers(iteration)._2 + " or in large scale: "+ numbers(iteration)._3)
-      }
+   // buildString("",0,input,1)
+    buildString("",0,input,1)
+  }
+  var resultA =""
+
+  val numbers = Array(Tuple2("",""),
+    Tuple2(" thousands and "," thousands and "),
+    Tuple2(" million "," million "),
+    Tuple2(" billion "," milliard "),
+    Tuple2(" trillion "," billion "),
+    Tuple2(" quadrillion "," billiard "),
+    Tuple2(" quintillion "," trillion "),
+    Tuple2(" sextillion "," trilliard ")
+  )
+  def getPosition(index:Int, shortOrLong:Int): String ={
+    shortOrLong match{
+      case 1 => numbers(index)._1
+      case 2 => numbers(index)._2
+    }}
+
+  def buildString(currentString:String,byThrees:Int,message:String,printExample:Int): Unit ={
+    var outPut = currentString
+    if (message.length > 3) {
+      outPut = message.substring(message.length - 3, message.length) + getPosition(byThrees,printExample) + outPut
+      val restOfMessage = message.substring(0, message.length - 3)
+      buildString(outPut, byThrees + 1, restOfMessage, printExample)
+    } else {
+      outPut = message + getPosition(byThrees,printExample) + outPut
+      println(outPut)
+      resultA= outPut
     }
-    test(0)
-
-
   }
 
 }
